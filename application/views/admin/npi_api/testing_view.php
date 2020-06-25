53034da052743cc3e693ed4ccb396dfa793d69ec
unknown <gurpreetsbscet@gmail.com>
2020-06-25 17:48:18 +0530

    Npi data view with datatable

---
+++ application/views/admin/npi_api/testing_view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Npi Bulk Data</title>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.13/datatables.min.css"/>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.13/datatables.min.js"></script> 
+    </head>
+    <body>
+    <h1>NPI DATA</h1>
+    <table id="book-table">
+<thead>
+<tr><td>NPI</td><td>Entity</td><td>Firstname</td><td>Lastname</td><td>Mobile</td></tr>
+</thead>
+<tbody>
+</tbody>
+</table>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#book-table').DataTable({
+        "ajax": {
+            url : "<?php echo site_url("admin/Npi_api/books_page") ?>",
+            type : 'GET'
+        },
+    });
+});
+</script>
+
+    </body>
+</html>
