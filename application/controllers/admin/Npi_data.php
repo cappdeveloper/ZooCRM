43c85bb89f6ec79b6cbe671e3cb0e3f8504ccbc7
unknown <gurpreetsbscet@gmail.com>
2020-06-26 12:22:29 +0530

    Bulk import with datatable

--- application/controllers/admin/Npi_data.php
+++ application/controllers/admin/Npi_data.php
@@ -12,6 +12,7 @@ class Npi_Data extends AdminController
         parent::__construct();
 
         $this->load->model('npi_model');
+
     }
 
     /* List all knowledgebase articles */
@@ -34,7 +35,7 @@ class Npi_Data extends AdminController
           $length = intval($this->input->get("length"));
 
           $books=$this->GetAPIData($start,$length);
-          
+          //echo json_encode($books);
            $a=$this->totalNPI();
 
           $data = array();
@@ -90,5 +91,86 @@ class Npi_Data extends AdminController
 
     }
 
+     public function NPI_page_bulk_import()
+    {
+        $this->load->helper('form');
+        //print_r($this->input->post()['res']);die;
+        $Successfully=0;
+       $failed=0;
+       $alredy_exist=0;
+        foreach($this->input->post()['res'] as $single){
+             
+            $this->db->select('id');
+            $this->db->from('tblleads');
+            $this->db->where('number', $single);
+            $id = $this->db->get()->result();
+            if(!empty($id[0]->id)){
+              $alredy_exist= $alredy_exist+1;
 
+            }else{
+           $this->db->select('*');
+           $this->db->from('tblnpi_bulk');
+           $this->db->where('NPI',$single);
+           $fetched_data = $this->db->get()->result();
+           $data_to_insert = array(
+        'hash'=>'test',
+        'name'=>$fetched_data[0]->FirstName.$fetched_data[0]->LastName,
+        'title'=>'test',
+        'company'=>'test',
+        'description'=>'test',
+        'country'=>$fetched_data[0]->PracticeCountry,
+        'zip'=>$fetched_data[0]->PracticePostal,
+        'city'=>$fetched_data[0]->PracticeCity,
+        'state'=>$fetched_data[0]->PracticeState,
+        'address'=>$fetched_data[0]->FirstPracticeAddress,
+        'assigned'=>'test',
+        'dateadded'=>date('Y-m-d H:i:s'),
+        'from_form_id'=>'test',
+        'status'=>1,
+        'source'=>3,
+        'lastcontact'=>date('Y-m-d H:i:s'),
+        'dateassigned'=>'',
+        'last_status_change'=>'test',
+        'addedfrom'=>'test',
+        'email'=>'test',
+        'website'=>'test',
+        'phonenumber'=>$fetched_data[0]->PracticeTelephone,
+        'date_converted'=>'',
+        'lost'=>'test',
+        'junk'=>'test',
+        'last_lead_status'=>'',
+        'is_imported_from_email_integration'=>'test',
+        'email_integration_uid'=>'test',
+        'is_public'=>'test',
+        'default_language'=>'test',
+        'client_id'=>'test',
+        'number'=>$fetched_data[0]->NPI
+    );
+        if($this->db->insert('tblleads',$data_to_insert)){
+           
+           $Successfully = $Successfully+1;
+        }else{
+         
+            $failed= $failed+1;
+        }
+    }
+
+    }
+    if(!empty($Successfully) && !empty($alredy_exist)){
+  $error = ['message'=>" $Successfully Records Imported Successfully and $alredy_exist Records already exists "];
+                echo json_encode($error);die;
+
+}
+if(!empty($Successfully)){
+  $error = ['message'=>"Records Imported Successfully"];
+                echo json_encode($error);die;
+
+}
+if(!empty($alredy_exist)){
+  $error = ['message'=>"Records alredy exists"];
+                echo json_encode($error);die;
+
+}
+
+}
 }
--- application/views/admin/npi_api/npi_data.php
+++ application/views/admin/npi_api/npi_data.php
@@ -23,10 +23,16 @@
                          ?> -->
 
              <head>
+              <button class="btn btn-success" id="bulk">Bulk Import Data</button>
         <title>Npi Bulk Data</title>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.13/datatables.min.css"/>
 <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.13/datatables.min.js"></script> 
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
     </head>
     <body>
     <h1>NPI DATA</h1>
@@ -48,7 +54,32 @@ $(document).ready(function() {
             url : "<?php echo site_url("admin/Npi_Data/NPI_page") ?>",
             type : 'GET'
         },
+          dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
     });
+    $(document).on('click','#bulk',function(){
+      var Npi_Number = [];
+      $('.sorting_1').each(function()
+        {
+          Npi_Number.push($(this).html()); 
+        });
+     $.ajax({
+      url:"<?php echo site_url("admin/Npi_Data/NPI_page_bulk_import") ?>",
+      type:'POST',
+      data: {res:Npi_Number},
+      success:function(result){
+
+        alert(result);
+
+      }
+     });
+    });
+  
 });
 </script>
 
