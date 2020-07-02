268e5de9fa3fb3c8c1578467d4b7948884ecbeb7
as200261@gmail.com <as200261@gmail.com>
2020-07-02 21:35:27 +0530

    Search+Datatable

--- application/views/admin/npi_api/table_html.php
+++ application/views/admin/npi_api/table_html.php
@@ -3,16 +3,47 @@
       $table_data = array(
         array(
           'name'=>_l('NPI'),
-          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-Name')
+          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-NPI')
         ),
         array(
-          'name'=>_l('LastName'),
+          'name'=>_l('First Name'),
+          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-FirstName')
+        ),
+         array(
+          'name'=>_l('Last Name'),
           'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-LastName')
         ),
          array(
-          'name'=>_l('FirstName'),
-          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-Firstname')
+          'name'=>_l('Taxnomy Name'),
+          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-TaxName')
         ),
+         array(
+          'name'=>_l('Practice Address'),
+          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-PAddress')
+        ),
+            array(
+          'name'=>_l('City'),
+          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-PCity')
+        ),
+            array(
+          'name'=>_l('State'),
+          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-PState')
+        ),
+           array(
+          'name'=>_l('Practice Phone'),
+          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-PPhone')
+        ),
+           array(
+          'name'=>_l('Business Phone'),
+          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-BPhone')
+        ),
+           array(
+          'name'=>_l('Country'),
+          'th_attrs'=>array('class'=>'toggleable', 'id'=>'th-country')
+        ),
+
+
+
 
       );
       render_datatable($table_data,'npi_data',
--- application/views/admin/tables/npi_data.php
+++ application/views/admin/tables/npi_data.php
@@ -4,15 +4,25 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 $aColumns = [
     'NPI',
-    'LastName',
     'FirstName',
+    'LastName',
+    'tbltaxnomy_value.Tax_Name',
+    'FirstPracticeAddress',
+    'PracticeCity',
+    'PracticeState',
+    'PracticeTelephone',
+    'BusinessTelephone',
+    'PracticeCountry'
                ];
 $sIndexColumn ='NPI' ;
 $sTable       = db_prefix() . 'npi_bulk';
 $where        = [
 
     ];
-$result = data_tables_init($aColumns, $sIndexColumn, $sTable, [], $where, [
+$join = [
+    'INNER JOIN ' . db_prefix() . 'taxnomy_value ON ' . db_prefix() . 'taxnomy_value.Tax_value = ' . db_prefix() . 'npi_bulk.TaxonomyCode'];
+
+$result = data_tables_init($aColumns, $sIndexColumn, $sTable,$join, $where, [
    'NPI',
     ]);
 $output  = $result['output'];
@@ -25,6 +35,7 @@ foreach ($rResult as $aRow) {
 
         $row[] = $_data;
     }
+
 
 
     $output['aaData'][] = $row;
